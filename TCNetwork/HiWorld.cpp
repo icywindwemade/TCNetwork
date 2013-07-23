0bbd0091093acdbc4298533321563727290a9230
Rick Cheng <rick.cheng@wemadeusa.com>
2013-07-23 16:05:40 -0700

    First Commit

--- TCNetwork/HiWorld.cpp
+++ TCNetwork/HiWorld.cpp
@@ -1,10 +1,13 @@
 //
-//  TCNetwork.h
-//  TCNetwork
+//  HiWorld.cpp
+//  libNetwork
 //
 //  Created by Rickie Cheng on 7/23/13.
 //  Copyright (c) 2013 Rick Cheng. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#include "HiWorld.h"
 
+HiWorld::HiWorld() {
+    printf("HiWorld ctor\n");
+}
---
+++ TCNetwork/HiWorld.h
@@ -0,0 +1,22 @@
+//
+//  HiWorld.h
+//  libNetwork
+//
+//  Created by Rickie Cheng on 7/22/13.
+//  Copyright (c) 2013 Rick Cheng. All rights reserved.
+//
+
+#ifndef libNetwork_HiWorld_h
+#define libNetwork_HiWorld_h
+#include "stdio.h"
+
+class HiWorld {
+
+public:
+    HiWorld();
+    ~HiWorld();
+    
+    inline void print() { printf("HelloWorld\n"); }
+};
+
+#endif
