b50011acf3ae6ddadfb4924375ad6b6a5a47b893
Rick Cheng <rick.cheng@wemadeusa.com>
2013-07-29 20:29:50 -0700

    Monitor connection constantly.

--- TCNetwork/Engine/NetworkMonitor.cpp
+++ TCNetwork/Engine/NetworkMonitor.cpp
@@ -1,5 +1,12 @@
 #include "NetworkMonitor.h"
 #include "NetworkManager.h"
+#include "OnlineHandler.h"
+
+
+
+#define CONNECT_CHECKUP_CYCLES  30   // cycles in the loop until next check
+
+//==============================================================================
 
 pthread_t MonThread = 0;
 pthread_mutex_t MonLock;
@@ -26,21 +33,42 @@ NNetworkMonitor * NNetworkMonitor::Instance() {
 
 void NNetworkMonitor::Run() {
     useconds_t nsinv = (int)1000000*(1.0/m_FrameTime); // nanosecond sleep
+    static int checkCycles = 0;
+    
     while (isRunning) {
-        //printf("Montior is running\n");
         usleep(nsinv);
         NNetworkManager::Instance()->Update( m_FrameTime );
+        
+        if( GOnlineInfo->IsLoaded() && checkCycles >= CONNECT_CHECKUP_CYCLES ) {
+            if (checkConnection) {
+                bool bConnected = checkConnection();
+                if( bNetworkOnline ^ bConnected ) {  // XOR for state changed
+                    if( OLErrorHandleCB != NULL ) {
+                        if ( bConnected ) {
+                            OLErrorHandleCB( ENEC_StateOnline, " connection is established.");
+                        } else {
+                            OLErrorHandleCB( ENEC_StateOffline, " connection is not present.");
+                        }
+                    }
+                    
+                    bNetworkOnline = bConnected;
+                }
+            }
+            checkCycles = 0;
+        }
+        checkCycles++;
     }
     printf("Montior stopped running\n");
 }
 
-bool NNetworkMonitor::Start() {
+bool NNetworkMonitor::Start( CheckConnectionMethod method ) {
    	int Result = pthread_create( &MonThread, 0, MonitorThreadProc, this );
 	if( Result != 0 )
     {
         isRunning = false;
     }
 
+    checkConnection = method;
     isRunning = true;
     return isRunning;
 }
--- TCNetwork/Engine/NetworkMonitor.h
+++ TCNetwork/Engine/NetworkMonitor.h
@@ -1,19 +1,23 @@
 #ifndef NETWORK_MONITOR_H
 #define NETWORK_MONTIOR_H
 
+typedef  bool (*CheckConnectionMethod) (void);
+
 class NNetworkMonitor {
     bool isRunning;
+    bool bNetworkOnline;
     int m_FrameTime;
+    CheckConnectionMethod checkConnection;
     
 public:
-    NNetworkMonitor(): isRunning(false), m_FrameTime(33)
+    NNetworkMonitor(): isRunning(false), bNetworkOnline(false), m_FrameTime(33), checkConnection(0)
     {}
     
     ~NNetworkMonitor()
     {}
     
     void Run();
-    bool Start();
+    bool Start( CheckConnectionMethod method=0);
     void Stop();
     static NNetworkMonitor * Instance();  // Singleton instance
 };
